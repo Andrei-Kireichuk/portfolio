0d0e9f88031a591c00df51ac81ddc1f4f8a8ab80
Andrei Kireichuk <a.k.kireichuk@gmail.com>
2021-07-14 10:27:03 +0300

    3c

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 gsap.registerPlugin(ScrollTrigger);
 
+window.onbeforeunload = function() {
+    window.scrollTo(0, 0);
+  };
+
 gsap.to ("#ball", {
-    y:-300, duration: 0.7, repeat:-1, yoyo: true, ease: "power1",
+    y:-200, duration: 0.7, repeat:-1, yoyo: true, ease: "power1",
 });
 gsap.to ("#ball", {
     rotation:360, duration: 3.5, repeat:-1, ease: "linear",
@@ -11,13 +15,27 @@ gsap.from("#shadow", {
     scale:1, duration:0.7, repeat:-1, yoyo: true, ease:"power1", opacity: 1
 });
 
-gsap.to ("#attractive", {
-    xPercent: -100, repeat:-1, duration:3, ease: "power1.in"
-})
+let tl = gsap.timeline({
+    delay: 0.5,
+    repeat: -1, 
+    repeatDelay: 1, // seconds between repeats
+    repeatRefresh: true, // invalidates on each repeat
+    smoothChildTiming: true,
+
+    defaults: { // children inherit these defaults
+      duration: 3,
+      ease: "none" 
+    },
+      });
+      tl.to("#attractive", {xPercent: -300, repeat:true, repeatRefresh: true, duration:2, ease: "bounce"},"+=0.5")
+      .to(".wave", {opacity: 1, duration:0.3, repeat:5, yoyo:true},0);
+
+
+
 
 gsap.to("#lake-3",{ scale:1.4, scrollTrigger:{
     trigger: "#lake-3",
-    start:"top bottom",
+    start:"bottom bottom",
     end: "+=1200",
     scrub: 1,
     id: "L-1",
@@ -43,26 +61,36 @@ gsap.to(".title__parallax",{ y: -400, scrollTrigger:{
 }});
 gsap.to(".moveright-screen",{ xPercent: -100, scrollTrigger:{
     trigger: "#trigger",
-    start:"300px top",
-    end: "+=20",
-    scrub: 2,
-    id: "L-1",
+    start:"400px top",
+    end: "+=10",
+    scrub: 3,
+    id: "L-2",
     //markers: true,
 }});
 gsap.to(".moveleft-screen",{ xPercent: 100, scrollTrigger:{
     trigger: "#trigger",
-    start:"500px top",
-    end: "+=30",
-    scrub: 1,
+    start:"800px top",
+    end: "+=50",
+    scrub: 3,
     id: "L-1",
     //markers: true,
 }});
 gsap.to(".container__three",{ scrollTrigger:{
     trigger: ".container__three",
     start:"top top",
-    end: "+=700",
+    end: "+=1200",
     scrub: 0,
     id: "L-1",
     pin: true,
-    markers: true,
+    //markers: true,
+}});
+
+gsap.from("#v-shot",{y:-1050, 
+    scrollTrigger:{
+        trigger:".container-4",
+        start:"top bottom",
+        end:"bottom top",
+        scrub:0,
+        id:"L4",
+        markers: true,
 }});
